e875411945f22af393472ce88fc87b5b8b74b70d
hymndrazill <srihafiras@yahoo.fr>
2022-10-24 01:56:37 +0100

    adding the pages & styling

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,29 @@
+import './register.scss'
+
+const Register = () => {
+  return (
+    <div className='register'>
+      <div className="card">
+        <div className="left">
+            <h1>Social World.</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit quidem possimus perspiciatis aliquid eius facilis iure ullam nisi eveniet delectus dolor voluptas facere</p>
+            <span>Do You have an account?</span>
+            <button>Login</button>
+        </div>
+        <div className="right">
+        <h1>Register</h1>
+        <form>
+            <input type="text" placeholder='Username' />
+            <input type="email" placeholder='Email' />
+            <input type="text" placeholder='username' />
+            <input type="text" placeholder='Full Name' />
+            <input type="password" placeholder='Password' />
+            <button>Register</button>
+        </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Register
