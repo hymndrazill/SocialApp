caeb24d67acaad7aa3f6a3cc31a526f041c358ee
hymndrazill <srihafiras@yahoo.fr>
2022-11-01 01:49:28 +0100

    finished handling events from the login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,42 +1,66 @@
-import { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import { AuthContext } from '../../context/authContext'
-import './login.scss'
+import { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/authContext';
+import './login.scss';
 
 const Login = () => {
-  const {login} = useContext(AuthContext)
-  
-  const handleLogin =  () => {
+  const { login } = useContext(AuthContext);
+  const [error, seterror] = useState(null);
+  const [inputs, setInputs] = useState({
+    username: '',
+    password: '',
+  });
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    console.log(inputs);
+  };
+  const handleLogin = async (e) => {
+    e.preventDefault();
     try {
-      login();
-    } catch (e){
-      console.log(e)
+      await login(inputs);
+    } catch (e) {
+      seterror(e.response.data);
     }
-  }
-
+  };
 
   return (
     <div className='login'>
-      <div className="card">
-        <div className="left">
-            <h1>Hello World.</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit quidem possimus perspiciatis aliquid eius facilis iure ullam nisi eveniet delectus dolor voluptas facere</p>
-            <span>Don't You have an account?</span>
-            <Link to="/register">       
+      <div className='card'>
+        <div className='left'>
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Reprehenderit quidem possimus perspiciatis aliquid eius facilis iure
+            ullam nisi eveniet delectus dolor voluptas facere
+          </p>
+          <span>Don't You have an account?</span>
+          <Link to='/register'>
             <button>Register</button>
-            </Link>
+          </Link>
         </div>
-        <div className="right">
-        <h1>Login</h1>
-        <form>
-            <input type="text" placeholder='username' />
-            <input type="password" placeholder='password' />
+        <div className='right'>
+          <h1>Login</h1>
+          <form>
+            <input
+              onChange={handleChange}
+              type='text'
+              placeholder='username'
+              name='username'
+            />
+            <input
+              onChange={handleChange}
+              type='password'
+              placeholder='password'
+              name='password'
+            />
+            {error && error}
             <button onClick={handleLogin}>Login</button>
-        </form>
+          </form>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
