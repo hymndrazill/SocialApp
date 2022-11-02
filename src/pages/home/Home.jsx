d47cf6177f2c64e89e0558482234120313526048
hymndrazill <srihafiras@yahoo.fr>
2022-11-02 01:43:56 +0100

    added the share component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,13 @@ import Stories from '../../components/stories/Stories'
 import Posts from '../../components/posts/Posts'
 
 import './home.scss'
+import Share from '../../components/share/Share'
 
 const Home = () => {
   return (
     <div className='home'>
         <Stories/>
+        <Share/>
         <Posts/>      
 
     </div>
