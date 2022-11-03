0410951610a07358aa8bfe41dc832698b5cf2d28
hymndrazill <srihafiras@yahoo.fr>
2022-11-03 01:11:21 +0100

    added react queries and dynamic fetch

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,55 +1,98 @@
-import "./post.scss"
-import {useState} from "react"
-import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined"
-import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined"
-import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined"
-import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined"
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz"
-import {Link} from "react-router-dom"
-import Comments from "../comments/Comments"
-import moment from "moment"
-const Post = ( {post}) => {
-    const [commentsOpen, setCommentsOpen] = useState(false)
+import './post.scss';
+import { useState } from 'react';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
+import TextsmsOutlinedIcon from '@mui/icons-material/TextsmsOutlined';
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { Link } from 'react-router-dom';
+import Comments from '../comments/Comments';
+import moment from 'moment';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { makeRequest } from '../../axios';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/authContext';
+const Post = ({ post }) => {
+  const [commentsOpen, setCommentsOpen] = useState(false);
+  const { currentUser } = useContext(AuthContext);
 
+  const { isLoading, error, data } = useQuery(['likes', post.id], () =>
+    makeRequest.get('/likes/?postId=' + post.id).then((res) => {
+      return res.data;
+    })
+  );
+  const queryClient = useQueryClient();
+  // Mutations
+  const mutation = useMutation(
+    (liked) => {
+      if (liked) return makeRequest.delete('/likes/?postId='+post.id,);
+        return makeRequest.post('/likes', { postId: post.id });
+      
+    },
+    {
+      onSuccess: () => {
+        // Invalidate and refetch
+        queryClient.invalidateQueries({ queryKey: ['likes'] });
+      },
+    }
+  );
 
-    const liked = false;
+  const handleLike = (e) => {
+    e.preventDefault();
+    mutation.mutate(data.includes(currentUser.id));
+  };
   return (
-    <div className="post">
-        <div className="container">
-            <div className="user">
-                <div className="userInfo">
-                    <img src={post.profilePic} alt="" />
-                    <div className="details">
-                    <Link to={`/profile/${post.userId}`} style={{textDecoration:"none", color:"inherit"}}>
-                        <span className="name">{post.name}</span> 
-                    </Link>
-                        <span className="date">{moment(post.createdAt).fromNow()}</span>
-                    </div>
-                </div>
-                <MoreHorizIcon/>
+    <div className='post'>
+      <div className='container'>
+        {isLoading ? "loading":<>
+        <div className='user'>
+          <div className='userInfo'>
+            <img src={post.profilePic} alt='' />
+            <div className='details'>
+              <Link
+                to={`/profile/${post.userId}`}
+                style={{ textDecoration: 'none', color: 'inherit' }}
+              >
+                <span className='name'>{post.name}</span>
+              </Link>
+              <span className='date'>{moment(post.createdAt).fromNow()}</span>
             </div>
-            <div className="content">
-                    <p>{post.desc}</p>
-                    <img src={"./upload/" + post.img} alt="" />
-            </div>
-            <div className="info">
-                <div className="item">
-                 {liked ? <FavoriteOutlinedIcon/> : <FavoriteBorderOutlinedIcon/>}
-                 12 Likes.
-                </div>
-                <div className="item" onClick={()=> setCommentsOpen(!commentsOpen)}>
-                  <TextsmsOutlinedIcon/> 
-                 10 Comments.
-                </div>
-                <div className="item">
-                 <ShareOutlinedIcon/> 
-                 Share
-                </div>
-            </div>
-            { commentsOpen && <Comments postId={post.id}/> }
+          </div>
+          <MoreHorizIcon />
         </div>
+        <div className='content'>
+          <p>{post.desc}</p>
+          <img src={'./upload/' + post.img} alt='' />
+        </div>
+        <div className='info'>
+          <div className='item'>
+            {error ? error :
+            isLoading ? (
+              '...'
+            ) : data.includes(currentUser.id) ? (
+              <FavoriteOutlinedIcon
+                style={{ color: 'red' }}
+                onClick={handleLike}
+              />
+            ) : (
+              <FavoriteBorderOutlinedIcon onClick={handleLike} />
+            )}
+            {data?.length} likes
+          </div>
+          <div className='item' onClick={() => setCommentsOpen(!commentsOpen)}>
+            <TextsmsOutlinedIcon />
+            10 Comments.
+          </div>
+          <div className='item'>
+            <ShareOutlinedIcon />
+            Share
+          </div>
+        </div>
+        {commentsOpen && <Comments postId={post.id} />}
+        </>}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
